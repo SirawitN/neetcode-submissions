9ad0e21ebb5984273f9e296a14d64d00f2e73421
SirawitN <70020229+SirawitN@users.noreply.github.com>
2026-06-13 20:59:40 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.go
@@ -0,0 +1,19 @@
+func binSearch(nums []int, target int, start, stop int) int {
+	if start > stop {
+		return -1
+	}
+
+	mid := start + (stop-start)/2
+	if nums[mid]==target {
+		return mid
+	}
+	
+	if nums[mid] > target {
+		return binSearch(nums, target, start, mid-1)
+	}
+	return binSearch(nums, target, mid+1, stop)
+}
+
+func search(nums []int, target int) int {
+	return binSearch(nums, target, 0, len(nums)-1)
+}
