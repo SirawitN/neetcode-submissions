457d246ad7282bbd8c0a4e80015351fb21b761a7
SirawitN <70020229+SirawitN@users.noreply.github.com>
2026-08-29 11:23:28 +0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.go
@@ -0,0 +1,19 @@
+import (
+    "maps"
+    "slices"
+)
+
+func groupAnagrams(strs []string) [][]string {
+    results := make(map[[26]int][]string, len(strs))
+
+    for _, str := range strs {
+        charFreq := [26]int{}
+        for _, r := range str {
+            charFreq[r - 'a'] += 1
+        }
+
+        results[charFreq] = append(results[charFreq], str)
+    }
+
+    return slices.Collect(maps.Values(results))
+}
