9c9ae37628cd043caeb98cb65e1ea05205f4be54
SirawitN <70020229+SirawitN@users.noreply.github.com>
2026-09-01 10:29:18 +0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.go
@@ -0,0 +1,33 @@
+import (
+    "maps"
+    "slices"
+)
+
+func topKFrequent(nums []int, k int) []int {
+    elementFreq := make(map[int]int, len(nums))
+
+    for _, n := range nums {
+        elementFreq[n] += 1
+    }
+
+    keys := slices.Collect(maps.Keys(elementFreq))
+    fmt.Println(keys)
+    slices.SortFunc(keys, func(a, b int) int {
+        aFreq, bFreq := elementFreq[a], elementFreq[b]
+        if aFreq != bFreq {
+            if aFreq > bFreq {
+                return -1
+            }
+            return 1
+        }
+
+        if a < b{
+            return -1
+        } else if a > b{
+            return 1
+        }
+        return 0
+    })
+
+    return keys[:k]
+}
