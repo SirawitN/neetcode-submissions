0689792774de181b5f9cf9738d5a78f1141fa9c9
SirawitN <70020229+SirawitN@users.noreply.github.com>
2026-09-03 09:40:16 +0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.go
@@ -0,0 +1,17 @@
+func twoSum(numbers []int, target int) []int {
+	var sum int
+	i, j := 0, len(numbers)-1
+
+	for i < j{
+		sum = numbers[i]+numbers[j]
+		if (sum==target){
+			break
+		} else if (sum<target) {
+			i += 1
+		} else {
+			j -= 1
+		}
+	}
+
+	return []int{i+1, j+1}
+}
